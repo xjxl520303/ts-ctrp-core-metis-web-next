b757526a1a6ff479db3bedbf633d227fcb20257e
吴杰(01421670) <wujie792@sf-express.com>
2023-04-04 15:53:41 +0800

    fix: 忘记提交路由文件

--- src/router/routes/index.ts
+++ src/router/routes/index.ts
@@ -1,5 +1,11 @@
 import type { RouteRecordRaw } from 'vue-router'
 import { workbenchRoute } from './workbench'
+import {
+  EXCEPTION_COMPONENT,
+  LAYOUT,
+  PAGE_NOT_FOUND_NAME,
+  REDIRECT_NAME,
+} from '@/constants'
 
 export const RootRoute: RouteRecordRaw = {
   path: '/',
@@ -19,9 +25,53 @@ export const AboutRoute: RouteRecordRaw = {
   component: () => import('@/views/about/index.vue'),
 }
 
+// 404
+export const NotFoundRoute: RouteRecordRaw = {
+  path: '/:path(.*)*',
+  name: PAGE_NOT_FOUND_NAME,
+  component: LAYOUT,
+  meta: {
+    title: 'ErrorPage',
+  },
+  children: [
+    {
+      path: '/:path(.*)*',
+      name: PAGE_NOT_FOUND_NAME,
+      component: EXCEPTION_COMPONENT,
+      meta: {
+        title: 'ErrorPage',
+        hideBreadcrumb: true,
+        hideMenu: true,
+      },
+    },
+  ],
+}
+
+// 重定向
+export const RedirectRoute: RouteRecordRaw = {
+  path: '/redirect',
+  component: LAYOUT,
+  name: 'RedirectTo',
+  meta: {
+    title: REDIRECT_NAME,
+  },
+  children: [
+    {
+      path: '/redirect/:path(.*)',
+      name: REDIRECT_NAME,
+      component: () => import('@/views/sys/redirect/index.vue'),
+      meta: {
+        title: REDIRECT_NAME,
+      },
+    },
+  ],
+}
+
 export const appRoutes = [
   LoginRoute,
   RootRoute,
   AboutRoute,
   workbenchRoute,
+  NotFoundRoute,
+  RedirectRoute,
 ]
