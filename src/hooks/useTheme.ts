e28f7a287924616c89b13ba26525a7cb3cd47ba9
吴杰(01421670) <wujie792@sf-express.com>
2023-04-24 11:19:42 +0800

    fix: missing element plus dark css

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -14,5 +14,7 @@ export function toggleDark() {
   const curHtmlCls = document.getElementsByTagName('html')[0].getAttribute('class')
   const clsWithTheme = cls(curHtmlCls ?? '', { dark: !isDark.value })
   document.getElementsByTagName('html')[0].setAttribute('class', clsWithTheme)
+  if (isDark.value)
+    import.meta.glob('element-plus/theme-chalk/dark/css-vars.css', { eager: true })
   toggle()
 }
